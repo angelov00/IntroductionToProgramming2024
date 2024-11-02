50c250143612cf69d827a669a87c9e57e49e59c3
Martin Angelov <147129698+angelov00@users.noreply.github.com>
2024-11-02 08:48:39 +0200

    fix task06

--- week05 - Arrays/solutions/Task06.cpp
+++ week05 - Arrays/solutions/Task06.cpp
@@ -1,30 +1,38 @@
 #include <iostream>
 
-int removeDuplicates(int arr[], int size) {
-    if (size <= 1) return size;
+const unsigned MAX_SIZE = 64;
 
-    int uniqueElements = 1;
+void removeDuplicates(int arr[], unsigned& size) {
+    if (size <= 1) return;
+
+    int uniqueElements[MAX_SIZE];
+    uniqueElements[0] = arr[0];
+    int uniqueCount = 1;
 
     for (int i = 1; i < size; ++i) {
         bool isDuplicate = false;
 
         //Проверяваме дали arr[i] вече съществува в уникалната част на масива
-        for (int j = 0; j < uniqueElements; ++j) {
-            if (arr[i] == arr[j]) {
+        for (int j = 0; j < uniqueCount; ++j) {
+            if (arr[i] == uniqueElements[j]) {
                 isDuplicate = true;
                 break;
             }
         }
 
         if (!isDuplicate) {
-            arr[uniqueElements] = arr[i];
-            ++uniqueElements;
+           uniqueElements[uniqueCount++] = arr[i];
         }
     }
-    return uniqueElements; 
+
+    for (int i = 0; i < uniqueCount; ++i) {
+        arr[i] = uniqueElements[i];
+    }
+    size = uniqueCount;
+
 }
 
-void printArray(const int arr[], int size) {
+void printArray(int arr[], const unsigned size) {
     for (int i = 0; i < size; ++i) {
         std::cout << arr[i] << " ";
     }
@@ -33,16 +41,16 @@ void printArray(const int arr[], int size) {
 
 int main() {
 
-    const unsigned size = 9;
-    int numbers[] = { 1, 2, 3, 2, 4, 5, 1, 3, 6 }; 
+    unsigned size = 9;
+    int numbers[MAX_SIZE] = { 1, 2, 3, 2, 4, 5, 1, 3, 6 };
 
     std::cout << "Original array: ";
     printArray(numbers, size);
 
-    int newSize = removeDuplicates(numbers, size);
+    removeDuplicates(numbers, size);
 
     std::cout << "Array after removing duplicates: ";
-    printArray(numbers, newSize);
+    printArray(numbers, size);
 
     return 0;
 }
