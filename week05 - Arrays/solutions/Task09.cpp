ec95394474a2d9853639c930a35fa56cc7dd1918
Martin Angelov <147129698+angelov00@users.noreply.github.com>
2024-11-02 09:19:14 +0200

    a minor fix

--- week05 - Arrays/solutions/Task09.cpp
+++ week05 - Arrays/solutions/Task09.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 
+const unsigned MAX_SIZE = 64;
 const unsigned ALPHABET_SIZE = 26;
 
 void countLetters(char arr[], unsigned size) {
@@ -19,8 +20,8 @@ void countLetters(char arr[], unsigned size) {
 
 int main() {
 
-	const unsigned size = 8;
-	char arr[size] = { 'a', 'b', 'b', 'b', 'd', 'm', 'm', 'y' };
+	unsigned size = 8;
+	char arr[MAX_SIZE] = { 'a', 'b', 'b', 'b', 'd', 'm', 'm', 'y' };
 
 	countLetters(arr, size);
 	return 0;
