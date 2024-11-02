6632599227d89f112fc3a650a2ceac50ffc0f178
Martin Angelov <147129698+angelov00@users.noreply.github.com>
2024-11-02 09:15:53 +0200

    a minor fix

--- week05 - Arrays/solutions/Task07.cpp
+++ week05 - Arrays/solutions/Task07.cpp
@@ -2,11 +2,11 @@
 
 const unsigned MAX_SIZE = 64;
 
-void insertElement(int arr[], int& size, int index, int value, int maxSize) {
+void insertElement(int arr[], int& size, int index, int value) {
 
     // Проверка дали индексът е валиден
-    if (index < 0 || index > size || size >= maxSize) {
-        std::cout << "Invalid index or array is full." << std::endl;
+    if (index < 0 || index > size || size >= MAX_SIZE) {
+        std::cout << "Invalid index or full array!" << std::endl;
         return;
     }
 
@@ -20,7 +20,6 @@ void insertElement(int arr[], int& size, int index, int value, int maxSize) {
     ++size; // Увеличаване на размера на масива
 }
 
-// Функция за извеждане на масива
 void printArray(const int arr[], int size) {
     for (int i = 0; i < size; ++i) {
         std::cout << arr[i] << " ";
@@ -29,7 +28,7 @@ void printArray(const int arr[], int size) {
 }
 
 int main() {
-   
+
     int numbers[MAX_SIZE] = { 1, 2, 3, 4, 5 };
     int size = 5;
 
@@ -39,7 +38,7 @@ int main() {
     int index = 2; // Индекс, на който да вмъкнем новото число
     int value = 99; // Число за вмъкване
 
-    insertElement(numbers, size, index, value, MAX_SIZE);
+    insertElement(numbers, size, index, value);
 
     std::cout << "Array after insertion: ";
     printArray(numbers, size);
